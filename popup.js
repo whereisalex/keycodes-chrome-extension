df4882aa53b0c49f3dfb2ecd1d0f27b36432cbe1
Alexandra <alexandra.geier@ringieraxelspringer.ch>
2021-04-16 22:53:07 +0200

    add keycode functionality

--- popup.js
+++ popup.js
@@ -1,24 +1,9 @@
-// Initialize button with user's preferred color
-let changeColor = document.getElementById("changeColor");
-
-chrome.storage.sync.get("color", ({ color }) => {
-  changeColor.style.backgroundColor = color;
+document.addEventListener('keydown', event => {
+    event.preventDefault();
+    document.getElementById('instruction').innerHTML = '';
+    document.getElementById('keycode').innerHTML = event.keyCode | event.which;
+    document.getElementById('code').innerHTML = event.code;
+    document.getElementById('key').innerHTML = event.key;
+    document.getElementById('location').innerHTML = event.location;
+    document.getElementById('more-info').style.display = 'flex';
 });
-
-// When the button is clicked, inject setPageBackgroundColor into current page
-changeColor.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-  
-    chrome.scripting.executeScript({
-      target: { tabId: tab.id },
-      function: setPageBackgroundColor,
-    });
-  });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
